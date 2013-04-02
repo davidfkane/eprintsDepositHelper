bac1654100e9b027cbf9723633a68948f7ea8057
David Kane <dkane@wit.ie>
2013-04-02 12:53:24 +0100

    Tidying up the home page to make it easier for people to see what's where.

--- index.php
+++ index.php
@@ -12,10 +12,14 @@
 <ul>
 <li><a href="addfile.php">Add file example</a></li>
 <li><a href="createNewEPrint.php">Create New EPrint example</a></li>
-<li><a href="/depositforms">Deposit Forms</a></li>
+<li><a href="/depositforms/deposit_article.php">Caltech Deposit Form Example</a></li>
+<li><a href="/depositforms/simpleuse.php">Simple Action Page Example, with pre-filled variables</a> (for developers to get a quick handle on how the class is implemented)</li>
 </ul>
 <hr/>
+
 <p>This is an implementation of the php CURL functionality in EPrints.</p>
+
+<!--
 <pre>
     
           <file id='http://witeprints/id/file/213'>
@@ -43,7 +47,10 @@ Li4uIGdlbmVyYWxseSBwYXJzaW5nIGl0IHdpdGggcmVndWxhciBleHByZXNzaW9ucwo/Pg==
     
     
 </pre>
+-->
+
 <?php
+/*
 $hash = "f00c59a0339482631528e5e07dd99ccd";
 
 $encoded = "PD9waHAKLy8gaW5jbHVkZSBjbGFzcyBmaWxlCnJlcXVpcmUgJ2NVUkwucGhwJzsKCi8vIGluc3RhbnRpYXRlCiRjdXJsID0gbmV3IEN1cmwoMSk7CgovLyBXZSBwYXNzIHZhcmlhYmxlcyBpbiBQT1NUCiRjdXJsLT5hZGRQb3N0VmFyKCdsb2dpbicsJ2N5cmlsJyk7CiRjdXJsLT5hZGRQb3N0VmFyKCdwYXNzd29yZCcsJ3MzY3VyZVBAc3N3MHJkJyk7CgovLyBDb25uZWN0aW9uIHRvIGZhY2Vib29rLCBhIGNvb2tpZSBpcyBjcmVhdGVkIGFuZCBzdG9yZWQgaW4gYSBmaWxlCiRjdXJsLT5leGVjKCdodHRwOi8vd3d3LmZhY2Vib29rLmNvbS9sb2cucGhwJyk7CgovLyBOb3cgd2UgY2FuIGdldCBhIHBhZ2UgYXMgYSBsb2dnZWQgdXNlcgokdCA9ICRjdXJsLT5leGVjKCdodHRwOi8vd3d3LmZhY2Vib29rLmNvbS9wcm9maWxlLnBocCcpOwoKLy8gQW5kIGRvIHdoYXQgd2Ugd2FudCB3aXRoIHRoZSByZXN1bHQgLi4uIGdlbmVyYWxseSBwYXJzaW5nIGl0IHdpdGggcmVndWxhciBleHByZXNzaW9ucwo/Pg==";
@@ -52,7 +59,7 @@ print  "<br/>";
 print  "Filesize = " . strlen(base64_decode($encoded)) . "<br/>";
 print $hash . "<br/>";
 print md5(base64_decode($encoded)) .  "<br/>";
-
+*/
 ?>
 
 
